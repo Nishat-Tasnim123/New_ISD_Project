c6410a3731cf03c24cc0eebc413851ac72ccc047
Fatema Mehnaz Promitee <fatema.promitee@gmail.com>
2020-09-17 22:16:31 +0600

    Add Product Item dialog designed

--- app/src/main/java/com/example/doorsteptailors/ProductPricing.java
+++ app/src/main/java/com/example/doorsteptailors/ProductPricing.java
@@ -1,23 +1,30 @@
 package com.example.doorsteptailors;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class ProductPricing extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_pricing);
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
         try{
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }catch (NullPointerException e){
             System.out.println(e.getMessage());
         }
         //for tab bar
+
+        ListView productList = findViewById(R.id.productList);
+        ViewGroup.LayoutParams params = productList.getLayoutParams();
+        params.height = 0 ;
+        productList.setLayoutParams(params);
     }
 
     @Override
